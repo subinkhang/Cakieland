83022b5d9082a9574b019cfc0b981c9af2c8aeae
subinkhang <subinkhang102@gmail.com>
2024-04-15 18:48:53 +0700

    add component productList & improve app
    -install Axios
    -install Vue

---
+++ Cakieland/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>home</title>
+
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <div id="app">
+            <home-component></home-component>
+        </div>
+    </body>
+</html>
